41ba41ebc09ff816d36c35f9e42d8f993ade63cc
Raul26-tech <rauletec2020@gmail.com>
2024-12-02 02:39:17 -0300

    "Retirando configuração do Jest"

--- src/framework/db/redis-cofig.ts
+++ src/framework/db/redis-cofig.ts
@@ -1,9 +1,9 @@
 import Redis from "ioredis";
 
 const redisClient = new Redis({
-  host: process.env.REDIS_HOST, // Ou o nome do serviço no Docker Compose (ex: 'redis')
-  port: Number(process.env.REDIS_PORT), // Porta padrão do Redis
-  retryStrategy: (times) => Math.min(times * 50, 2000), // Estratégia de reconexão
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  retryStrategy: (times) => Math.min(times * 50, 2000),
 });
 
 export default redisClient;
